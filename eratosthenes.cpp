a09970aef58556c327632cf68b4cefc665833757
alyona <a1enakrasi1nikova@ya.ru>
2024-04-02 19:31:24 +0300

    task2

--- eratosthenes.cpp
+++ eratosthenes.cpp
@@ -2,7 +2,6 @@
 
 std::vector<int> sieve(int n) {
 	bool* flags = new bool[n + 1];
-	//memset(flags, true, sizeof(flags));
 	for (int i = 0; i < n + 1; i++)
 		flags[i] = true;
 	for (int p = 2; p * p <= n; p++) {
