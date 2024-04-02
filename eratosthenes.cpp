949f1f5bd887490b604837850572dd8d09fbb170
alyona <a1enakrasi1nikova@ya.ru>
2024-04-02 18:49:24 +0300

    task2

---
+++ eratosthenes.cpp
@@ -0,0 +1,19 @@
+#include "eratosthenes.h"
+using namespace std;
+
+std::vector<int> sieve(int n) {
+	bool* prime = new bool[n + 1];
+	memset(prime, true, sizeof(prime));
+
+	for (int p = 2; p * p <= n; p++) {
+		if (prime[p]) {
+			for (int i = p * p; i <= n; i += p)
+				prime[i] = false;
+		}
+	}
+	vector<int> primes;
+	for (int p = 2; p <= n; p++)
+		if (prime[p])
+			primes.push_back(p);
+    return primes;
+}
