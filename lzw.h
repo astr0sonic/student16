2c4c342d170721039a247869559924afe40e858f
alyona <a1enakrasi1nikova@ya.ru>
2024-05-06 15:05:03 +0300

    task10

---
+++ lzw.h
@@ -0,0 +1,7 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <unordered_map>
+
+std::vector<int> compress(const std::string& text);
+std::string decompress(const std::vector<int>& compressed);
